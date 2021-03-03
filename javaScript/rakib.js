d3c0219b95a716ebde5200494b8cdc9f2a8c877c
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-03 10:00:07 +0100

    last work

--- javaScript/rakib.js
+++ javaScript/rakib.js
@@ -1,91 +1,107 @@
-console.log("rakib is gona insah allah be a programer");
-// document.write("rakib is gona insah allah be a programer");
+// console.log("rakib is gona insah allah be a programer");
+// // document.write("rakib is gona insah allah be a programer");
 
-var sure_name="rakib";
-console.log(sure_name);
-// document.write("<hr>");
-// document.write(sure_name);
+// var sure_name="rakib";
+// console.log(sure_name);
+// // document.write("<hr>");
+// // document.write(sure_name);
 
-var age = 30;
-console.log("hi my name is "+sure_name+"and I am "+age+" years old!");
-
-
-
-var team= ["rakib", "mike","rakin","sumon"];
-console.log(team[2]);
-var fruit = ["banana","Apple","Orange","strawberry "];
-
-console.log(fruit.sort());
-
-var output="hello world rakib";
-
-// document.write(output.substr(4,5)); // output "o wor" only five charcter
-
-if (true){
-    var varVariable= "this is var";
-
-}
-
-console.log(varVariable);
-if(true){
-    let letVariable= "this is let"
-console.log(varVariable);
-
-}
-// console.log(letVariable);
-var age_rubi =5;
-function f(){
-    alert("Foo \n bar");
-    console.log(age_rubi);
-}
-// console.log(a);
-// f();
-const colors = ['blue', 'yellow', 'pink'];
-let age_abdulla= 54;
-
-// colors = [ 'green', 'purple', 'brown'];
-
-// const colors=['yellow', 'pink']
- colors.push('Black');
- console.log(colors);
-
-
-console.log(typeof(age_abdulla.toString()));
-
-console.log("this is my country".length);
-
-function student_age(age){
-    console.log("stdent_age is "+age);
-}
-
-student_age(32);
+// var age = 30;
+// console.log("hi my name is "+sure_name+"and I am "+age+" years old!");
 
 
 
-let  a= "this is my village and name barishal";
+// var team= ["rakib", "mike","rakin","sumon"];
+// console.log(team[2]);
+// var fruit = ["banana","Apple","Orange","strawberry "];
 
-// console.log(a.indexOf('s'));
+// console.log(fruit.sort());
 
-let temp=new Array();
-temp=a.split(" ");
-// console.log(temp);
+// var output="hello world rakib";
 
-console.log(temp[1]);
+// // document.write(output.substr(4,5)); // output "o wor" only five charcter
 
-let  x= "this is my village";
-// console.log(x.substring(2,4));
-// console.log(x.substring(3));
+// if (true){
+//     var varVariable= "this is var";
 
-// console.log(x.toUpperCase());
-// console.log(x.toLowerCase());
+// }
 
-let two_d_array=[["1","2","3"],
-                 ["4","5","6"],
-                 ["7","8","9","10"]];
+// console.log(varVariable);
+// if(true){
+//     let letVariable= "this is let"
+// console.log(varVariable);
+
+// }
+// // console.log(letVariable);
+// var age_rubi =5;
+// function f(){
+//     alert("Foo \n bar");
+//     console.log(age_rubi);
+// }
+// // console.log(a);
+// // f();
+// const colors = ['blue', 'yellow', 'pink'];
+// let age_abdulla= 54;
+
+// // colors = [ 'green', 'purple', 'brown'];
+
+// // const colors=['yellow', 'pink']
+//  colors.push('Black');
+//  console.log(colors);
+
+
+// console.log(typeof(age_abdulla.toString()));
+
+// console.log("this is my country".length);
+
+// function student_age(age){
+//     console.log("stdent_age is "+age);
+// }
+
+// student_age(32);
+
+
+
+// let  a= "this is my village and name barishal";
+
+// // console.log(a.indexOf('s'));
+
+// let temp=new Array();
+// temp=a.split(" ");
+// // console.log(temp);
+
+// console.log(temp[1]);
+
+// let  x= "this is my village";
+// // console.log(x.substring(2,4));
+// // console.log(x.substring(3));
+
+// // console.log(x.toUpperCase());
+// // console.log(x.toLowerCase());
+
+// let two_d_array=[["1","2","3"],
+//                  ["4","5","6"],
+//                  ["7","8","9","10"]];
 
 // console.log(two_d_array[2][3]);
 
 // console.log((two_d_array.pop()));
 
-two_d_array.push();
-console.log(two_d_array);
+// two_d_array.push();
+// console.log(two_d_array);
+
+
+// var fruits = ["Banana", "Kiwi", "Apple", "Watermelon"];
+
+// console.log(fruits.slice(2,3));
+// var n= "4";
+// console.log(n.number());
+
+
+let two_d_array=[["1","2","3"],
+                  ["4","5","6"],
+                 ["7","8","9","10"]];
+console.log(two_d_array[0][1]);
+console.log("\n");          
+console.log(two_d_array[0][2]);
+
