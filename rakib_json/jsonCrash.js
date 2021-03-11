ecc5a39d13e7ff0c93ef734a642eb4f418597a59
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-11 01:58:04 +0100

    learning json

---
+++ rakib_json/jsonCrash.js
@@ -0,0 +1,98 @@
+
+// var fs=require('fs');
+// // var fs=require('faker/locale/sk');
+// var faker=require('faker');
+
+// // const { faker } = require("faker");
+
+
+// var peoples=[];
+
+// for (let i = 0; i <10; i++) {
+  
+//     var people={};
+//     people.name=faker.name.findName();
+//     people.email=faker.internet.email();
+//     people.address={
+
+//         city:faker.address.city(),
+//         country:faker.address.country(),
+//         lat:faker.address.latitude(),
+//         long:faker.address.longitude()
+//     };
+//     peoples.push(people);
+// }
+
+// // console.log(JSON.stringify(peoples));
+// fs.watchFile('demo.json',JSON.stringify(peoples),function(err,data){
+// console.log("file created Successfully");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [ {
+//     "name":"abdulla rakib",
+//     "email":"rakib0751@gmail.com",
+//     "links":[
+//         {"facebook":"https//fb.com"},
+//         {"linkedIn":"https//linkedIn.com"},
+//         {"twitter":"https//twitter.com"}
+//        ],
+//    "skills":["java","javaScript","Node.js"],
+//    "age":32,
+//    "Ismarried":true 
+
+// },
+// {
+//    "name":"abdulla rakin",
+//    "email":"rakin0751@gmail.com",
+//    "links":[
+//        {"facebook":"https//fb.com"},
+//        {"linkedIn":"https//linkedIn.com"},
+//        {"twitter":"https//twitter.com"}
+//       ],
+//   "skills":["html","javaScript","Node.js"],
+//   "age":23,
+//   "Ismarried":false 
+
+// }]
